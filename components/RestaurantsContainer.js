032e80a634feecfdb4369d37254278577f27182c
matthewmjm <matthewmjm@yahoo.com>
2020-12-15 15:49:38 -0700

    search bar functionality

--- components/RestaurantsContainer.js
+++ components/RestaurantsContainer.js
@@ -1,10 +1,11 @@
-import React, { useEffect } from 'react';
-import { View, Text, StyleSheet, ScrollView } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { View, Text, StyleSheet, ScrollView, TextInput, ImageBackgroundComponent, Button } from 'react-native';
 import { useDispatch, useSelector } from 'react-redux';
 import RestaurantCard from './RestaurantCard';
 
 
 const apiKey = 
+
 const termValue = "restaurants"
 const locationValue = "denver"
 const apiUrl = `https://api.yelp.com/v3/businesses/search?term=${termValue}&location=${locationValue}`
@@ -14,6 +15,7 @@ export default function RestaurantsContainer() {
     // const dispatch = store.dispatch
     const dispatch = useDispatch()
     const restaurants = useSelector(state => state.restaurants)
+    const [searchTerm, setSearchTerm] = useState('')
 
     useEffect(() => {
     fetch(apiUrl, {
@@ -33,10 +35,33 @@ export default function RestaurantsContainer() {
         />
     })
 
+    const handleSearchText = (text) => {
+        setSearchTerm(text)
+    }
+    const handleSearch = (text) => {
+        const updatedURL = `https://api.yelp.com/v3/businesses/search?term=${termValue}&location=${searchTerm}`
+
+        fetch(updatedURL, {
+            headers: {
+                "Authorization": `Bearer ${apiKey}`
+            }
+        })
+        .then(response => response.json())
+        .then(({businesses}) => dispatch({type: "SET_RESTAURANTS", restaurants: businesses}))
+    }
+
     return (
-        <ScrollView style={styles.container}>
-            {showRestaurants()}
-        </ScrollView>
+        <>
+            <TextInput style={styles.textInputBox} onChangeText={handleSearchText} value={searchTerm} />
+            <Button 
+                onPress={handleSearch}
+                title='Search'
+
+            />
+            <ScrollView style={styles.container}>
+                {showRestaurants()}
+            </ScrollView>
+        </>
     )
 }
 
@@ -44,5 +69,12 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         margin: 15,
+    },
+    textInputBox: {
+        // flex: 1,
+        height: 40,
+        borderColor: 'gray',
+        borderWidth: 1,
+        backgroundColor: '#dedddc'
     }
 });
